fe564bdc7a5d93000838bb5cbb6d75c4f5e4b9e3
Vasilka Vlaykova <vasilkavlaykova@yahoo.com>
2026-02-06 19:15:30 +0000

    Clean script and finalise branch version

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Running from test branch")
+
+
+
+def greet(name):
+    return f"Hello, {name}. Practising Git with Python!"
+if __name__=="__main__":
+    user_name = input("Enter your name:")
+    print(greet(user_name))
